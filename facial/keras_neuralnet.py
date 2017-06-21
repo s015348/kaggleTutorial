4b481b10b377a3a29d37de2394499e497fd99bce
yangtian <yangtian@tsinghua.org.cn>
2017-06-21 14:13:20 +0800

    updating to match keras2

--- facial/keras_neuralnet.py
+++ facial/keras_neuralnet.py
@@ -14,34 +14,34 @@ def neuralnet1(imput_dim):
     neuralnet.add(Dense(100, input_dim=imput_dim))
     neuralnet.add(Activation('relu'))
     neuralnet.add(Dense(30))
-    neuralnet.compile(loss='mean_squared_error', 
+    neuralnet.compile(loss='mean_squared_error',
                       optimizer=SGD(lr=0.01, momentum=0.9, nesterov=True))
     return neuralnet
 
 
 def neuralnet2(input_shape):
     neuralnet = Sequential()
-    
-    neuralnet.add(Convolution2D(32, 3, 3, input_shape=input_shape))
+
+    neuralnet.add(Convolution2D(32, (3, 3), input_shape=input_shape))
     neuralnet.add(Activation('relu'))
     neuralnet.add(MaxPooling2D(pool_size=(2, 2)))
-    
-    neuralnet.add(Convolution2D(64, 2, 2))
+
+    neuralnet.add(Convolution2D(64, (2, 2)))
     neuralnet.add(Activation('relu'))
     neuralnet.add(MaxPooling2D(pool_size=(2, 2)))
-    
-    neuralnet.add(Convolution2D(128, 2, 2))
+
+    neuralnet.add(Convolution2D(128, (2, 2)))
     neuralnet.add(Activation('relu'))
     neuralnet.add(MaxPooling2D(pool_size=(2, 2)))
-    
+
     neuralnet.add(Flatten())
     neuralnet.add(Dense(500))
     neuralnet.add(Activation('relu'))
     neuralnet.add(Dense(500))
     neuralnet.add(Activation('relu'))
     neuralnet.add(Dense(30))
-    
-    neuralnet.compile(loss='mean_squared_error', 
+
+    neuralnet.compile(loss='mean_squared_error',
                       optimizer=SGD(lr=0.01, momentum=0.9, nesterov=True))
     return neuralnet
 
@@ -77,14 +77,14 @@ def predict(neuralnet, data, history, image_size, features):
     # RMSE is sqrt of last 'valid_loss' with scale back to image size
     last_valid_loss = history.history['val_loss'][-1]
     score = np.sqrt(last_valid_loss) * image_size /2
-    
+
     # Data has to be reshaped to 4 dimensions
     predict = neuralnet.predict(data)
     predict = (predict + 1) * image_size / 2
-    
+
     # Submission format is different to predict
     submission = convert_to_submission_format(features, predict)
-    
+
     return submission, pd.DataFrame(predict, columns=features), score
 
 
@@ -100,5 +100,5 @@ def reshape_data(train, test, image_size):
         train = train.reshape(-1, image_size, image_size, 1)
         test = test.reshape(-1, image_size, image_size, 1)
     else:
-        print("ERROR: please check constant KERAS_BACKEND which should be either th or tf")    
+        print("ERROR: please check constant KERAS_BACKEND which should be either th or tf")
     return train, test
